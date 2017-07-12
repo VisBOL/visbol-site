41c1d198833819330cb0ab811755ddf6fa1d470a
Zach Zundel <me@zachzundel.com>
2017-07-12 13:05:58 +0100

    Inital move

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var Design = require('./lib/design'),
+    Vec2 = require('./lib/geom/vec2'),
+    Rect = require('./lib/geom/rect'),
+    Matrix = require('./lib/geom/matrix'),
+    getDisplayList = require('./lib/getDisplayList')
+
+module.exports = {
+    Design: Design,
+    Vec2: Vec2,
+    Rect: Rect,
+    Matrix: Matrix,
+    getDisplayList: getDisplayList
+};
+
+
+
+
